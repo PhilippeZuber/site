7b8bd101c05dd4418a06ffe6912154b13992f2d2
PhilippeZuber <pzuber@hotmail.ch>
2018-06-13 16:40:42 +0200

    comment only

--- header.php
+++ header.php
@@ -94,7 +94,7 @@ $user = mysqli_fetch_assoc($result);
 $update_time = date_parse($user['update_time']);
 $last_update = $update_time['day'] . "." . $update_time['month'] . "." . $update_time['year'];
 ?>
-
+<!--Modal window for profile-->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="profil-wortlabor">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
