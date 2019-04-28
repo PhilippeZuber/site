b357dbb9959c92e1478c07f2a89fedf82294ed1e
PhilippeZuber <pzuber@hotmail.ch>
2019-04-28 22:28:51 +0200

    clip bachelor embed

--- about.php
+++ about.php
@@ -35,17 +35,20 @@ $page = 'about';
                             <div class="alert alert-success"><span class="glyphicon glyphicon-wrench" aria-hidden="true"></span> Die Passwörter werden neu verschlüsselt abgespeichert.</div>
                             <div class="alert alert-success"><span class="glyphicon glyphicon-gift" aria-hidden="true"></span> In den letzten Monaten sind zahlreiche neue Bilder und Wörter dazugekommen.</div>
                         </div>
-                    </div><!--row-->
+                    </div><!--/row-->
                     <div class="row">
                         <div class="col-md-8">
-                        <div class="page-header">
-                            <h1>Philippe & Jacqueline Zuber</h1>
+                            <div class="page-header">
+                                <h1>Philippe & Jacqueline Zuber</h1>
+                            </div>
+                                <p class="lead">Wortlab ist ein Projekt von Philippe Zuber mit der Unterstützung von Jacqueline Zuber. Mit Wortlab führen wir unsere Berufe – Multimediaproducer & Logopädin, zusammen. Unser Ziel ist es Logopädinnen und anderen Interessierten den Arbeitsalltag zu erleichtern und möglichst viele Kinder davon profitieren zu lassen.</p>
+                                <p><strong>«Wörter die mit x anfangen»</strong> vielleicht haben Sie bereits genau diese Phrase in eine herkömmliche Suchmaschine eingegeben und wurden früher oder später fündig. Wortlab will das Suchen nach bestimmten Wörtern und Bildern vereinfachen. Um dies zu ermöglichen ist Wortlab auf die Mitarbeit vieler Benutzer angewiesen. Sie selbst können Wörter einsenden, die wiederum von Administratoren eingepflegt werden.</p>
+                                <p>Wortlab befindet sich zurzeit in einer Testphase. D.h. die Datenbank von Wortlab ist noch unvollständig, weshalb einige Filterkriterien noch nicht funktionieren. Sollte sich herausstellen, dass eine Nachfrage an Wortlab besteht, wird die Entwicklung weitergeführt.</p>
+                            <!--<div class="embed-responsive embed-responsive-16by9">
+                                <iframe src="https://player.vimeo.com/video/282533727?title=0&byline=0&portrait=0 fullscreen" allowfullscreen></iframe>
+                            </div>-->
                         </div>
-						    <p class="lead">Wortlab ist ein Projekt von Philippe Zuber mit der Unterstützung von Jacqueline Zuber. Mit Wortlab führen wir unsere Berufe – Multimediaproducer & Logopädin, zusammen. Unser Ziel ist es Logopädinnen und anderen Interessierten den Arbeitsalltag zu erleichtern und möglichst viele Kinder davon profitieren zu lassen.</p>
-                            <p><strong>«Wörter die mit x anfangen»</strong> vielleicht haben Sie bereits genau diese Phrase in eine herkömmliche Suchmaschine eingegeben und wurden früher oder später fündig. Wortlab will das Suchen nach bestimmten Wörtern und Bildern vereinfachen. Um dies zu ermöglichen ist Wortlab auf die Mitarbeit vieler Benutzer angewiesen. Sie selbst können Wörter einsenden, die wiederum von Administratoren eingepflegt werden.</p>
-                            <p>Wortlab befindet sich zurzeit in einer Testphase. D.h. die Datenbank von Wortlab ist noch unvollständig, weshalb einige Filterkriterien noch nicht funktionieren. Sollte sich herausstellen, dass eine Nachfrage an Wortlab besteht, wird die Entwicklung weitergeführt.</p>
-                        </div>
-                    </div>
+                    </div><!--/row-->
                 </div><!--/container-->		
             </div><!--/content-->
         </div><!--/wrapper-->
