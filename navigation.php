c3aa63af0ff1678319d2158b6f0145b885ad7043
PhilippeZuber <pzuber@hotmail.ch>
2019-11-17 13:24:17 +0100

    responsive top navigation

--- navigation.php
+++ navigation.php
@@ -5,8 +5,11 @@
                 <i class="glyphicon glyphicon-align-left"></i>
                 <span>Sidebar</span>
             </button>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <i class="glyphicon glyphicon-align-justify"></i>
+            </button>
         </div>
-        <div class="collapse navbar-collapse"  id="bs-example-navbar-collapse-1">
+        <div id="navbar" class="collapse navbar-collapse" aria-expanded="false" style="height: 1px;">
             <?php
                 if ($_SESSION['role'] == 1 OR $_SESSION['role'] == 2) {/*visible for logged-in only*/
             ?>
